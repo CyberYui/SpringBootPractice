b937bc5a872f4efe261b72997e9e35269b9e7700
Hirasawa_Yui <YuiNozomi@vip.qq.com>
2021-06-02 18:51:04 +0800

    springboot learning practice
    trying to catch an error , here is the first ini files

---
+++ exceptionCatch/src/main/java/com/cyber/exceptionCatch/ExceptionCatchApplication.java
@@ -0,0 +1,13 @@
+package com.cyber.exceptionCatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionCatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionCatchApplication.class, args);
+	}
+
+}
---
+++ exceptionCatch/src/test/java/com/cyber/exceptionCatch/ExceptionCatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.exceptionCatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionCatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
