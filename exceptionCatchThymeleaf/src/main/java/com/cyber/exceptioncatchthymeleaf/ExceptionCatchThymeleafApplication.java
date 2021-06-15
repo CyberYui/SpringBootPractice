938ebbdcbf3fd135d0731bf3005a3b53733edb46
Hirasawa_Yui <YuiNozomi@vip.qq.com>
2021-06-15 18:14:54 +0800

    upload new proj files
    Upload my new exceptionCatch project and using thymeleaf things

---
+++ exceptionCatchThymeleaf/src/main/java/com/cyber/exceptioncatchthymeleaf/ExceptionCatchThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cyber.exceptioncatchthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionCatchThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionCatchThymeleafApplication.class, args);
+    }
+
+}
---
+++ exceptionCatchThymeleaf/src/test/java/com/cyber/exceptioncatchthymeleaf/ExceptionCatchThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.exceptioncatchthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionCatchThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
